6a377da5989a13d4b590a2a77b2edcd31feaf107
thyoon515 <thyoon515@gmail.com>
2022-05-08 00:16:14 -0400

    commit tree 3c5bd5a07d314176c8d83eab88e76d197948d7a7

--- main.js
+++ main.js
@@ -3,9 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function hideError(){
+  let div = document.querySelector('div')
+  div.classList.add('hidden')
+}
+hideError()
 
+const modal = document.querySelector('#modal')
+const modalMessage = document.querySelector('#modal-message')
 
+const hearts = document.querySelectorAll('.like-glyph');
 
+hearts.forEach(hearts => hearts.addEventListener('click', heartClicked))
+
+function heartClicked(hearts){
+  mimicServerCall()
+  .then(() => {
+    if(hearts.target.innerText === EMPTY_HEART){
+      hearts.target.innerText = FULL_HEART;
+      hearts.target.classList.add('activated-heart')
+    }
+    else if(hearts.target.innerText === FULL_HEART){
+      hearts.target.innerText = EMPTY_HEART
+      hearts.target.classList.remove('activated-heart')
+    }
+  })
+  .catch(() => {
+    modal.classList.remove('hidden')
+    modalMessage.innerHTML = serverErrorMessage
+    setTimeout(() => {
+      modal.classList.add('hidden')
+    }, 3000)
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
